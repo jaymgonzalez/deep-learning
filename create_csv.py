90e82a73e6e2a85885e46584397d7584b514cfa1
Jay M Gonzalez <jaimungon@gmail.com>
2023-05-25 18:05:59 +0200

    csv

---
+++ create_csv.py
@@ -0,0 +1,12 @@
+import os
+
+os.makedirs(os.path.join("..", "data"), exist_ok=True)
+data_file = os.path.join("..", "data", "house_tiny.csv")
+with open(data_file, "w") as f:
+    f.write(
+        """NumRooms,RoofType,Price
+    NA,NA,127500
+    2,NA,106000
+    4,Slate,178100
+    NA,NA,140000"""
+    )
---
+++ read_csv.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = pd.read_csv("house_tiny.csv")
+print(data)
