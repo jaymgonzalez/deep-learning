9c92548f60fd3a52a97eb87a044cd6f71c57b8ab
Jay M Gonzalez <jaimungon@gmail.com>
2023-05-26 11:57:01 +0200

    add csv tensors

--- read_csv.py
+++ read_csv.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import torch
 
 data = pd.read_csv("house_tiny.csv")
 print(data)
+
+
+inputs, targets = data.iloc[:, 0:2], data.iloc[:, 2]
+inputs = pd.get_dummies(inputs, dummy_na=True)
+print(inputs)
+
+inputs = inputs.fillna(inputs.mean())
+print(inputs)
+
+X, y = torch.tensor(inputs.values), torch.tensor(targets.values)
+
+print(X, y)
